4f374ded8362ac81538cbbd42c143a87d078d41e
Sukreeth Vandrangi <79745093+155-Sukreeth@users.noreply.github.com>
2026-08-02 15:00:12 +0530

    refactor(web): centralize embedded assets in web package

--- cmd/wtm/main.go
+++ cmd/wtm/main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"embed"
 	"fmt"
 	"os"
 
 	"github.com/155-Sukreeth/webhook-time-machine/internal/cli"
+	"github.com/155-Sukreeth/webhook-time-machine/web"
 )
 
-//go:embed web/*
-var webFS embed.FS
-
 func main() {
-	rootCmd := cli.NewRootCmd(webFS)
+	rootCmd := cli.NewRootCmd(web.Assets)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error executing wtm: %v\n", err)
 		os.Exit(1)
